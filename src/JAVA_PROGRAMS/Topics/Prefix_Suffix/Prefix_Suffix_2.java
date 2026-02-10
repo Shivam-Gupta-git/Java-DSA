75f37688408ff0300c5150d1ba5d1bb975abd8d8
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2026-02-10 23:10:33 +0530

    Prefix

--- src/JAVA_PROGRAMS/Topics/Prefix_Suffix/Prefix_Suffix_2.java
+++ src/JAVA_PROGRAMS/Topics/Prefix_Suffix/Prefix_Suffix_2.java
@@ -1,4 +1,19 @@
 package JAVA_PROGRAMS.Topics.Prefix_Suffix;
 
+import java.util.Arrays;
+
 public class Prefix_Suffix_2 {
+    public static int[] prefixSum(int[] num){
+        for (int i = 1; i < num.length; i++){
+            num[i] += num[i - 1];
+        }
+        return  num;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 3, 5, 4, 6};
+        int[] result = prefixSum(numbers);
+
+        System.out.println(Arrays.toString(result));
+    }
 }
